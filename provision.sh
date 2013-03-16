1a55f4843c14d3139413f722091bfc1b485d4066
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-16 03:06:49 +0000

    Adding vi to the installation

--- provision.sh
+++ provision.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y apache2
+sudo apt-get update
 sudo apt-get install -y ubuntu-desktop
+sudo apt-get install -y vim
+sudo apt-get install -y apache2
 startx
