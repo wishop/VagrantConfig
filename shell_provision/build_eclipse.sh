053e8a5467b57eed8de41568807a1dfe60f50823
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-23 23:37:09 +0000

    Fixing issue when downloading for the first time.

--- shell_provision/build_eclipse.sh
+++ shell_provision/build_eclipse.sh
@@ -9,25 +9,26 @@ if [ ! -d $ECLIPSE_DIR ]; then
 	echo "Installing Eclipse..."
         
 	#check if we already downloaded the Eclipse IDE archive
-        if [ ! -f "$ECLIPSE_FILE" ]; then
-                wget -c -O$ECLIPSE_FILE "$ECLIPSE_URL"
-	else
+	if [ ! -f $ECLIPSE_FILE ]; then
+		wget -c -O$ECLIPSE_FILE "$ECLIPSE_URL"
+	fi
+    
+	if [ -f $ECLIPSE_FILE ]; then
 		tar -xvf $ECLIPSE_FILE -C /opt
 		sudo chown -R vagrant $ECLIPSE_DIR
 		sudo chgrp -R vagrant $ECLIPSE_DIR
 		sudo chmod 777 $ECLIPSE_DIR/configuration
 
-        	#make it available to drag'n'drop to the Unity Launcher
-	        mkdir -p $HOME/.local/share/applications
-       		cat $DATA/eclipse > $HOME/.local/share/applications/opt_eclipse.desktop
-        	chmod +x $HOME/.local/share/applications/opt_eclipse.desktop
+		#make it available to drag'n'drop to the Unity Launcher
+		mkdir -p $HOME/.local/share/applications
+		cat $DATA/eclipse > $HOME/.local/share/applications/opt_eclipse.desktop
+		chmod +x $HOME/.local/share/applications/opt_eclipse.desktop
 
 		#Installing Git Plugins on Eclipse
 		$ECLIPSE_DIR/eclipse -application org.eclipse.equinox.p2.director -noSplash -repository http://download.eclipse.org/releases/juno -installIUs org.eclipse.egit.feature.group
-	
 		$ECLIPSE_DIR/eclipse -application org.eclipse.equinox.p2.director -noSplash -repository http://download.eclipse.org/releases/juno -installIUs org.eclipse.jgit.feature.group
 	fi
 
 	sudo apt-get install -y git
-        echo "Installing Eclipse... Finish."
+	echo "Installing Eclipse... Finish."
 fi
