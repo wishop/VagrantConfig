f238bde6791ae0f5105a36021a7cc935477b20c6
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-23 23:59:05 +0000

    Fixing Java Oracle detections :) Hurray......

--- shell_provision/build_java.sh
+++ shell_provision/build_java.sh
@@ -6,19 +6,19 @@
 
 DIR=$1
 . $DIR/conf/provision.conf
+. $CONF/java.conf
 
-JAVA_VERSION=javac -version 2>&1 | cut -c 1-9
-if [ "$JAVA_VERSION" != "javac 1.7" ]; then
+if [ ! -d $JAVA_DIR ]; then
 	echo "Installing Java..."
 
 	#remove existing versions
-        sudo apt-get purge -y openjdk-\*
+	sudo apt-get purge -y openjdk-\*
 	
 	sudo cat $DATA/java >> /etc/apt/sources.list
 	sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 5CB26B26
-	
+
 	sudo apt-get update
 	sudo apt-get install -y update-sun-jre
-	
+
 	echo "Installing Java... Finish."
 fi
