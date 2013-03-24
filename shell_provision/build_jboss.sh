ade9b60b78771136497957c11b5e54642f3f2940
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-24 23:45:55 +0000

    Creating JBoss users through the add-user.sh script

--- shell_provision/build_jboss.sh
+++ shell_provision/build_jboss.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-##################################################
-#                Install JBoss AS                #
-##################################################
+##############################################################################################
+#                                    Install JBoss AS                                        #
+#                                                                                            #
+# - https://docs.jboss.org/author/display/AS71/add-user+utility                              #
+# - https://docs.jboss.org/author/display/AS7/Deploying+the+Helloworld+example+using+Eclipse #                                                             #
+##############################################################################################
 
 DIR=$1
 . $DIR/conf/jboss.conf
@@ -16,10 +19,17 @@ if [ ! -d $JBOSS_DIR ]; then
 	fi
 	
 	if [ -f $JBOSS_FILE ]; then
+	
 		sudo mkdir $JBOSS_DIR
 		sudo tar -zxvf $JBOSS_FILE -C $JBOSS_DIR --strip-components=1
 		sudo chown -R vagrant $JBOSS_DIR
 		sudo chgrp -R vagrant $JBOSS_DIR
+		
+		# adding a management JBoss AS user
+		sudo $JBOSS_BIN/add-user.sh --silent jboss jb055mu > ${DOWNLOADS}/capture.log 2>&1
+		
+		# adding an application JBoss AS user
+		sudo $JBOSS_BIN/add-user.sh --silent -a jbossau jb055@u > ${DOWNLOADS}/capture.log 2>&1
 	fi
 	
 	echo "Installing JBoss AS... Finish."
