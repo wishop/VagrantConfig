87805f96e1481c21b796399d18f09335afce777a
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-24 19:29:10 +0000

    JBoss AS Initial commit. Downloading the file.

---
+++ shell_provision/build_jboss.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+##################################################
+#                Install JBoss AS                #
+##################################################
+
+DIR=$1
+. $CONF/jboss.conf
+
+if [ ! -d $JBOSS_DIR ]; then
+	echo "Installing JBoss AS..."
+
+	# check if we already downloaded the JBoss AS archive
+	if [ ! -f $JBOSS_FILE ]; then
+		wget -c -O$JBOSS_FILE "$JBOSS_URL"
+	fi
+	
+	if [ -f $JBOSS_FILE ]; then
+		tar -xvf $JBOSS_FILE -C /opt
+	fi 
+	
+	echo "Installing JBoss AS... Finish."
+fi
--- shell_provision/provision.sh
+++ shell_provision/provision.sh
@@ -18,6 +18,9 @@ $DIR/build_eclipse.sh $DIR
 # postgreSQL & postgreSQL Admin 3 build
 $DIR/build_postgresql.sh $DIR
 
+# JBoss AS build
+$DIR/build_jboss.sh $DIR
+
 # keep things pristine
 sudo apt-get -y autoremove
 sudo apt-get clean
