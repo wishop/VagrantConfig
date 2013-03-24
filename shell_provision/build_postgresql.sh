56e1bd3168e23cb9dd2486c9d317d5f260848c6c
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-24 17:58:02 +0000

    Postgres build. Initial commit. Done

---
+++ shell_provision/build_postgresql.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#########################################
+#   Install PostgresSQL, PSQL Admin     #
+#########################################
+
+DIR=$1
+. ${DIR}/conf/provision.conf
+. ${CONF}/provision.conf
+
+if [ -d $POSTGRES_DIR ]; then
+	echo "Installing postgreSQL..."
+
+	sudo apt-get install -y postgresql pgadmin3
+	sudo -u postgres psql -f ${SQL}/postgres.sql
+
+	echo "Installing postgreSQL... Finish."
+	
+fi
+
+
+
---
+++ shell_provision/sql/postgres.sql
@@ -0,0 +1,3 @@
+ALTER USER postgres with encrypted password 'p0stgr3s';
+CREATE DATABASE wishop WITH OWNER postgres;
+
