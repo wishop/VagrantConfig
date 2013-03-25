6b368ad4670b61c40e67c0f1cfc6ecd5619dcf21
Paulo Monteiro <paulo.from.portugal@gmail.com>
2013-03-25 17:09:21 +0000

    JBoss Configuration

--- shell_provision/build_jboss.sh
+++ shell_provision/build_jboss.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-##############################################################################################
-#                                    Install JBoss AS                                        #
-#                                                                                            #
-# - https://docs.jboss.org/author/display/AS71/add-user+utility                              #
-# - https://docs.jboss.org/author/display/AS7/Deploying+the+Helloworld+example+using+Eclipse #                                                             #
-##############################################################################################
+########################################################################################################################
+#                                    Install JBoss AS                                                                  #
+#                                                                                                                      #
+# - http://jdbc.postgresql.org/download.html                                                                           #
+# - http://jan.zawodny.pl/blog/2011/07/jboss-7-postgresql-9                                                            #
+# - https://docs.jboss.org/author/display/AS71/add-user+utility                                                        #
+# - https://docs.jboss.org/author/display/AS7/Deploying+the+Helloworld+example+using+Eclipse                           #
+# - http://stackoverflow.com/questions/14012549/how-to-install-postgresql-v9-1-and-jboss-as-v7-1-1-on-ubuntu-12-04-lts #
+########################################################################################################################
 
 DIR=$1
 . $DIR/conf/jboss.conf
@@ -22,14 +25,24 @@ if [ ! -d $JBOSS_DIR ]; then
 	
 		sudo mkdir $JBOSS_DIR
 		sudo tar -zxvf $JBOSS_FILE -C $JBOSS_DIR --strip-components=1
+		
+		# adding a management JBoss AS user - Currently this has a bug on JBoss's end
+		# a postInstall.sh will be created and will need to be executed from the CLI
+		# sudo $JBOSS_BIN/add-user.sh --silent jboss jb055mu
+		# sudo $JBOSS_BIN/add-user.sh --silent -a jbossau jb055au
+		sudo echo "./bin/add-user.sh --silent jboss jb055mu" > postInstall.sh
+		sudo echo "./bin/add-user.sh --silent -a jbossau jb055au" >> postInstall.sh
+		
+		# configuring the connection between JBoss and Postgres
+		sudo mkdir -p $JBOSS_POSTGRES
+		sudo cp $LIB/$POSTGRES_JDBC4 $JBOSS_POSTGRES
+		sudo cp $DATA/module.xml $JBOSS_POSTGRES
+		sudo cp $DATA/standalone.xml ${JOSS_CONFIG}/.
+		
+		# correcting the permissions
 		sudo chown -R vagrant $JBOSS_DIR
 		sudo chgrp -R vagrant $JBOSS_DIR
 		
-		# adding a management JBoss AS user
-		sudo $JBOSS_BIN/add-user.sh --silent jboss jb055mu > ${DOWNLOADS}/capture.log 2>&1
-		
-		# adding an application JBoss AS user
-		sudo $JBOSS_BIN/add-user.sh --silent -a jbossau jb055@u > ${DOWNLOADS}/capture.log 2>&1
 	fi
 	
 	echo "Installing JBoss AS... Finish."
